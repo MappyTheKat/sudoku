2413b6d94bbb05707bf808087c5d18932c283039
Mappy The Kat <mappythekat@naver.com>
2016-10-21 18:27:23 +0900

    일단 샘플 프로젝트 분리 뭔가 해보고싶으면WPFSample 여기서 해보세요

---
+++ Sample/WPFSample/WPFSample/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPFSample
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            xButtonBelow.Click += xButton_Pressed;
+
+            this.Loaded += MainWindow_Loaded;
+            this.xTextBoxTextInput.TextChanged += xTextBoxTextInputChanged;
+        }
+
+        void xTextBoxTextInputChanged(object sender, TextChangedEventArgs e)
+        {
+            var textbox = sender as TextBox;
+            xTextBlockHelloworld.Text = textbox.Text;
+
+        }
+
+        void MainWindow_Loaded(object sender, EventArgs e)
+        {
+            xTextBlockHelloworld.Text = "Loaded";
+        }
+
+        void xButton_Pressed(object sender, EventArgs e)
+        {
+            xTextBlockHelloworld.Text = "hahahahahahaha";
+        }
+    }
+}
