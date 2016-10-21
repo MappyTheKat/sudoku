fd22acb231087d8156cb573979d238d1a4d3c5f9
nhne <disxc21@hanmail.net>
2016-10-21 16:39:12 +0900

    complete, valid 구분해서 구현, backtracking 기초 코드 구현

--- Sudoku/Sudoku/MainWindow.xaml.cs
+++ Sudoku/Sudoku/MainWindow.xaml.cs
@@ -34,9 +34,9 @@ namespace Sudoku
         {
             var textbox = sender as TextBox;
             xTextBlockHelloworld.Text = textbox.Text;
-            
+
         }
-        
+
         void MainWindow_Loaded(object sender, EventArgs e)
         {
             xTextBlockHelloworld.Text = "Loaded";
@@ -47,7 +47,7 @@ namespace Sudoku
             xTextBlockHelloworld.Text = "hahahahahahaha";
 
             string ret = "";
-            
+
             ret = // invalid dummy puzzle Data!"
                 "1 2 3 4 5 6 7 8 9\n" +
                 "1 2 3 4 5 6 7 8 9\n" +
@@ -71,7 +71,7 @@ namespace Sudoku
                 "9 3 1 6 4 7 2 8 5\n";
             Board board;
             board = new Board(ret);
-            
+
             solve_backTrack(board);
             Thread.Sleep(1);
         }
@@ -79,7 +79,9 @@ namespace Sudoku
         void solve_backTrack(Board board)
         {
             Console.WriteLine("hello, Cruel World!");
-            board.Print_Board();
+            Console.WriteLine(board.get_board_string());
+            Console.WriteLine("valid : " + board.isValid());
+            Console.WriteLine("complete : " + board.isComplete());
             return;
         }
 
@@ -93,7 +95,7 @@ namespace Sudoku
         public Board(string str)
         {
             string[] parsed = str.Trim().Split();
-            if(parsed.Length != 81)
+            if (parsed.Length != 81)
             {
                 throw new System.ArgumentException("Parsing Error, Length failed.");
             }
@@ -101,76 +103,179 @@ namespace Sudoku
             boardData = new int[9, 9];
             gridSize = 9;
 
-            for(int i = 0; i < 9; i++)
+            for (int i = 0; i < 9; i++)
             {
-                for(int j = 0; j < 9; j++)
+                for (int j = 0; j < 9; j++)
                 {
                     boardData[i, j] = Int32.Parse(parsed[i * 9 + j]);
                 }
             }
         }
 
-        public void Print_Board()
-            // 내부 판정보 프린트
+        public string get_board_string()
+        // 내부 판정보 string 반환
         {
+            string a = "";
             for (int i = 0; i < gridSize; i++)
             {
-                for(int j = 0; j < gridSize; j++)
+                for (int j = 0; j < gridSize; j++)
                 {
-                    Console.Write(boardData[i, j]);
+                    a += boardData[i, j] + " ";
                 }
-                Console.WriteLine();
+                a += "\n";
             }
-            Console.WriteLine("valid : " + isValid());
+            return a;
+        }
+
+        public void solve()
+        {
+            Console.WriteLine("solving started");
+            module_backtrack mb = new module_backtrack(this);
+            mb.solve();
+            Console.WriteLine("Solving completed");
         }
 
         public bool isValid()
-            // board validation
         {
-            int sum, ir, ic, jr, jc;
+            //check wether board is valid
 
-            // 가로줄 확인
+            //row validate
             for(int i = 0; i < gridSize; i++)
             {
-                sum = 0;
-                for(int j = 0; j < gridSize; j++)
+                for(int j = 1; j <= gridSize; j++)
+                {
+                    int cnt = 0;
+                    for(int k = 0; k < gridSize; k++)
+                    {
+                        if (boardData[i, k] == j)
+                            cnt++;
+                    }
+                    //Console.WriteLine("row:{0}, checking {1}", i, j);
+                    if (cnt != 1)
+                        return false;
+                }
+            }
+
+            //column validate
+            for (int i = 0; i < gridSize; i++)
+            {
+                for (int j = 1; j <= gridSize; j++)
+                {
+                    int cnt = 0;
+                    for (int k = 0; k < gridSize; k++)
+                    {
+                        if (boardData[k, i] == j)
+                            cnt++;
+                    }
+                    if (cnt != 1)
+                        return false;
+                }
+            }
+
+            //grid validate
+            int width = (int) Math.Sqrt(gridSize);
+            int row, col;
+            for (int i = 0; i < gridSize; i++)
+            {
+                row = (i / width) * width;
+                col = (i % width) * width;
+                for (int j = 1; j <= gridSize; j++)
+                {
+                    int cnt = 0;
+                    for (int k = 0; k < gridSize; k++)
+                    {
+                        if (boardData[row + (k / width), col + (k % width)] == j)
+                            cnt++;
+                    }
+                    //Console.WriteLine("row:{0}, col:{1} checking {2} cnt:{3}", row, col, j, cnt);
+                    if (cnt != 1)
+                        return false;
+                }
+            }
+
+            return true;
+        }
+
+        public bool isComplete()
+        // check whether board is completed
+        {
+            if (!isValid())
+                return false;
+
+            int sum = 0;
+            for (int i = 0; i < gridSize; i++)
+            {  
+                for (int j = 0; j < gridSize; j++)
                 {
                     sum += boardData[i, j];
                 }
-                if (sum != gridSize * (gridSize + 1) / 2)
-                    return false;
             }
+            return sum == (gridSize + 1) * gridSize / 2 * gridSize;
+        }
+    }
 
-            // 세로줄 확인
+    public class module_backtrack
+    {
+        Board original;
+        Board copied;
+        int gridSize;
+
+        public module_backtrack(Board b)
+        {
+            original = b;
+            copied = new Board(b.get_board_string());
+            gridSize = copied.gridSize;
+        }
+
+        private int count_zero()
+        //copied 내부의 0 개수 반환
+        {
+            int c = 0;
             for (int i = 0; i < gridSize; i++)
             {
-                sum = 0;
                 for (int j = 0; j < gridSize; j++)
                 {
-                    sum += boardData[j, i];
+                    if (copied.boardData[i, j] == 0)
+                        c++;
                 }
-                if (sum != gridSize * (gridSize + 1) / 2)
-                    return false;
+            }
+            return c;
+        }
+
+        public bool solve()
+        {
+            if (!original.isValid())
+            {
+                Console.WriteLine("unsolvable puzzle: original data Invalid!");
+                return false;
+            }
+            int zero = count_zero();
+            return backtrack(zero);
+        }
+
+        public bool backtrack(int n)
+        {
+            if (n == 0)
+                return true;
+
+            for (int i = 0; i < gridSize; i++)
+            {
+                for (int j = 0; j < gridSize; i++)
+                {
+                    if (copied.boardData[i, j] != 0)
+                        continue;
+                    for (int k = 0; k < gridSize; k++)
+                    {
+                        copied.boardData[i, j] = k;
+                        if (copied.isValid())
+                        {
+                            return backtrack(n - 1);
+                        }
+                    }
+                }
             }
 
-            int sq = (int) Math.Sqrt(gridSize);
-            
-            // grid 내부 확인
-            for(int i = 0; i < gridSize; i++)
-            {
-                sum = 0;
-                ir = i / 3;
-                ic = i % 3;
-                for(int j = 0; j < gridSize; j++)
-                {
-                    jr = j / 3;
-                    jc = j % 3;
-                    sum += boardData[sq * ir + jr, sq * ic + jc];
-                }
-                if (sum != gridSize * (gridSize + 1) / 2)
-                    return false;
-            }
-            return true;
+            return false;
         }
     }
 }
