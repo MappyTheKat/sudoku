152f33aa830f79be757172e08de86b3d8eab10af
nhne <disxc21@hanmail.net>
2016-10-27 15:30:23 +0900

    아니 왜 안되지??

---
+++ Sudoku/Sudoku/Generator.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sudoku
+{
+    public class Generator
+    {
+        Board initBoard;
+        Generator(int size)
+        {
+            string init = "";
+            for(int i = 0; i < size; i++)
+            {
+                for(int j = 0; j < size; j++)
+                {
+                    init += "0 ";
+                }
+            }
+            initBoard = new Sudoku.Board(init);
+
+        }
+
+        public Board generate()
+        {
+            Board newBoard = initBoard.Copy();
+
+            //lasvegas algorithms.
+            int[] visited;
+            int numberSelect = 12;
+
+            return newBoard;
+        }
+    }
+}
